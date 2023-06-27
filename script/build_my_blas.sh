14f8fba70d3d64e5db21979ac2487bca51183ad6
wudu98 <1150269136@qq.com>
2023-06-27 14:18:40 +0800

    init

--- script/build_my_blas.sh
+++ script/build_my_blas.sh
@@ -4,7 +4,7 @@ set -e
 module switch lang/tcsds-1.2.37
 
 tmp=`dirname $0`
-PROJECT_ROOT=`cd $tmp; pwd`
+PROJECT_ROOT=`cd $tmp/..; pwd`
 cd ${PROJECT_ROOT}
 
 cd ./src
---
+++ test/test.py
@@ -0,0 +1,49 @@
+import time
+import torch
+
+print(torch.__version__)
+dtype=torch.float32
+
+loop = 10
+P  = 128
+H  = 7168
+S  = 2048
+NH = 56
+MB = 1
+MP = 1
+
+# Layer 1
+batch=1
+M=S
+N=P * NH * 3 / MP
+K=H
+
+print("Layer1 : M %d, N %d, K %d" % (M, N, K))
+Layer1_input  = torch.randn(batch, M, K, dtype=dtype)
+Layer1_wight  = torch.randn(batch, K, N, dtype=dtype)
+Layer1_output = torch.zero(batch, M, N, dtype=dtype)
+
+start = time.time()
+for i in range(loop):
+    Layer1_output = torch.bmm(Layer1_input, Layer1_wight)
+end = time.time()
+
+print("Layer1 : %.4f ms" % (1000 * (end - start) / loop))
+
+# Layer 2
+batch=NH / MP
+M=S
+N=S
+K=P
+
+print("Layer2 : M %d, N %d, K %d" % (M, N, K))
+Layer2_input  = torch.randn(batch, M, K, dtype=dtype)
+Layer2_wight  = torch.randn(batch, K, N, dtype=dtype)
+Layer2_output = torch.zero(batch, M, N, dtype=dtype)
+
+start = time.time()
+for i in range(loop):
+    Layer2_output = torch.bmm(Layer2_input, Layer2_wight)
+end = time.time()
+
+print("Layer2 : %.4f ms" % (1000 * (end - start) / loop))
