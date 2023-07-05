ae69aa7f413caeb51ba65d32fe145e09e99bbb95
wudu98 <1150269136@qq.com>
2023-07-05 20:56:34 +0800

    07052056

--- benchmark/batch_gemm_benchmark.c
+++ benchmark/batch_gemm_benchmark.c
@@ -33,7 +33,7 @@ void my_blas_batch_sgemm(const int parallel_mode, const int batch_count, const i
 	{
 		int ncores = omp_get_num_threads();
 		for(int i = 0; i < batch_count; i++){
-			int nteams = 4;
+			int nteams = 12;
 			#pragma omp target teams num_teams( nteams ) thread_limit( ncores/nteams )
 			{
 				int team = omp_get_team_num();
