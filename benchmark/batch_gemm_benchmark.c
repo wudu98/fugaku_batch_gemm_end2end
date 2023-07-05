ed185541db73f90ffcd30ee80bc7e3861b9dbebb
wudu98 <1150269136@qq.com>
2023-07-05 19:16:46 +0800

    07051916

--- benchmark/batch_gemm_benchmark.c
+++ benchmark/batch_gemm_benchmark.c
@@ -31,12 +31,12 @@ void my_blas_batch_sgemm(const int parallel_mode, const int batch_count, const i
 	}
 	else if (parallel_mode == 2 )
 	{
+		int nteams = 3;
 		for(int i = 0; i < batch_count; i++){
-			#pragma omp target teams num_teams( 3 ) thread_limit( 12 / 3 )
-			#pragma omp distribute
+			#pragma omp target teams num_teams( nteams ) thread_limit( 12 / nteams )
+			// #pragma omp distribute
 			for(int j = 0; j < batch_size[i]; j+=batch_size[i] / 3){
 				int team = omp_get_team_num();
-				int nteams = omp_get_num_teams(); 
 				printf("Team %d out of %d teams\n", team, nteams);
 				cblas_sgemm(layout, transa, transb, m[i], n[i], k[i], alpha[i], a[batch_head[i]+j+team], lda[i], b[batch_head[i]+j+team], ldb[i], beta[i], c[batch_head[i]+j+team], ldc[i]);
 			}
